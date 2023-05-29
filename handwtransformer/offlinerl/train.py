f079a27c5bac249291070b6d1c8dd7a223db8f69
Tobias Jacob <tobi.jacob@gmx.net>
2023-05-29 15:52:03 +0200

    Better sampling

--- handwtransformer/offlinerl/train.py
+++ handwtransformer/offlinerl/train.py
@@ -17,39 +17,41 @@ def train(config: Config):
     batch_size = 32
     pred_window = 32
     preview_batch_size = 8
+    train_sequences_mask = None
     
     summary_writer = SummaryWriter()
     
     for step in range(1000):
         for mode in ["train", "train", "train", "train", "eval"]:
             split_i = len(sequences) // 10 * 8
+            
             if mode == "train":
                 batch_indices = torch.randint(0, split_i, (batch_size,))
             else:
                 batch_indices = torch.randint(split_i, len(sequences), (batch_size,))
-            predict_cutoff = torch.randint(0, sequences.shape[1] - pred_window, (1,))
+    
+            while train_sequences_mask is None or train_sequences_mask.sum() == 0:
+                predict_cutoff = torch.randint(0, sequences.shape[1] - pred_window, (1,))
+                batch_tokens = tokens[batch_indices]
+                batch_tokens_mask = tokens_mask[batch_indices]
+                batch_sequences_so_far = sequences[batch_indices, :predict_cutoff]
+                batch_sequences_so_far_mask = sequences_mask[batch_indices, :predict_cutoff]
+                train_sequences = sequences[batch_indices, predict_cutoff:predict_cutoff + pred_window]
+                train_sequences_mask = sequences_mask[batch_indices, predict_cutoff:predict_cutoff + pred_window]
             
-            batch_tokens = tokens[batch_indices]
-            batch_tokens_mask = tokens_mask[batch_indices]
-            batch_sequences_so_far = sequences[batch_indices, :predict_cutoff]
-            batch_sequences_so_far_mask = sequences_mask[batch_indices, :predict_cutoff]
-            train_sequences = sequences[batch_indices, predict_cutoff:predict_cutoff + pred_window]
-            train_sequences_mask = sequences_mask[batch_indices, predict_cutoff:predict_cutoff + pred_window]
-            
-            if train_sequences_mask.sum() > 0:
-                if mode == "eval":
-                    with torch.no_grad():
-                        model.eval()
-                        loss = model(batch_tokens, batch_tokens_mask, batch_sequences_so_far, batch_sequences_so_far_mask, train_sequences, train_sequences_mask)
-                else:
-                    model.train()
+            if mode == "eval":
+                with torch.no_grad():
+                    model.eval()
                     loss = model(batch_tokens, batch_tokens_mask, batch_sequences_so_far, batch_sequences_so_far_mask, train_sequences, train_sequences_mask)
-                    optim.zero_grad()
-                    loss.backward()
-                    optim.step()
-                print(step, loss.item())
-                summary_writer.add_scalar(f"{mode}/loss", loss.item(), step)
-                    
+            else:
+                model.train()
+                loss = model(batch_tokens, batch_tokens_mask, batch_sequences_so_far, batch_sequences_so_far_mask, train_sequences, train_sequences_mask)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+            print(step, mode, loss.item())
+            summary_writer.add_scalar(f"{mode}/loss", loss.item(), step)
+                
             if step % 10 == 9 and mode == "eval":
                 # generate some samples
                 model.eval()
